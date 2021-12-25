ca6191449e731b59ab37933296c6f222127d9eb0
e-doschechnikova <evadrobakova@gmail.com>
2021-12-25 22:59:11 +0300

    studying functions, arrow functions

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let num = 20;
+function showFirstMessage(text) {
+  console.log(text);
+  console.log(num);
+}
+
+showFirstMessage("Hello world!");
+showFirstMessage("Hello");
+console.log(num);
+
+function calc(a, b) {
+  return a + b;
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
+
+function ret() {
+  let num = 50;
+  return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+const logger = function () {
+  console.log("hello");
+};
+
+logger();
